340c32ab94f2bd825c3cb4b28049472b37bbba76
ZzangWoo <whdlwns20@gmail.com>
2020-12-13 20:56:30 +0900

    Delegate 예제

--- Delegate_Example/Delegate_Example/Program.cs
+++ Delegate_Example/Delegate_Example/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace Delegate_Example
 {
+    delegate int TestDelegate(int a, int b);
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator();
+            TestDelegate testDelegate;
+
+            testDelegate = new TestDelegate(calculator.Plus);
+            Console.WriteLine(testDelegate(3, 4));
+
+            testDelegate = new TestDelegate(Calculator.Minus);
+            Console.WriteLine(testDelegate(7, 5));
         }
     }
 }
