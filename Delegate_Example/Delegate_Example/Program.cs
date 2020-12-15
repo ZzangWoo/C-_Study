63a3d96e9388d3cee2654e7c6dc8a77c222a847b
ZzangWoo <whdlwns20@gmail.com>
2020-12-15 16:36:50 +0900

    Delegate Study

--- Delegate_Example/Delegate_Example/Program.cs
+++ Delegate_Example/Delegate_Example/Program.cs
@@ -21,9 +21,76 @@ namespace Delegate_Example
         }
     }
 
+    class ClassEx2
+    {
+        public static int AscendCompare(int a, int b)
+        {
+            if (a > b) return 1;
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        public static int DescendCompare(int a, int b)
+        {
+            if (a < b) return 1;
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        public static void BubbleSort(int[] dataSet, Compare compare)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i < dataSet.Length - 1; i++)
+            {
+                for (j = 0; j < dataSet.Length - (i + 1); j++)
+                {
+                    if (compare(dataSet[j], dataSet[j + 1]) > 0) {
+                        temp = dataSet[j + 1];
+                        dataSet[j + 1] = dataSet[j];
+                        dataSet[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    delegate int Compare(int a, int b);
+
     class Program
     {
         static void Main(string[] args)
+        {
+            Example2();
+        }
+
+        static void Example2()
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("-- 오름차순 정렬 --");
+            ClassEx2.BubbleSort(array, new Compare(ClassEx2.AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+            Console.WriteLine("\n-- 내림차순 정렬 --");
+            ClassEx2.BubbleSort(array2, new Compare(ClassEx2.DescendCompare));
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($"{array2[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Example1()
         {
             Calculator calculator = new Calculator();
             TestDelegate testDelegate;
