500e5e45026edc4a48823fe9d7c6c085a91de7e5
ZzangWoo <whdlwns20@gmail.com>
2020-12-17 09:32:14 +0900

    1217 study

---
+++ Attribute_Example/Attribute_Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute_Example
+{
+    class ExampleClass1
+    {
+        [Obsolete("Deprecated")]
+        public static void OldMethod()
+        {
+            Console.WriteLine("Old Method");
+        }
+
+        public static void NewMethod()
+        {
+            Console.WriteLine("New Method");
+        }
+    }
+
+    public static class ExampleClass2
+    {
+        public static void WriteLine(string message,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0,
+            [CallerMemberName] string member = "")
+        {
+            Console.WriteLine($"{file}(Line:{line}) {member}: {message}\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        private static void Test1()
+        {
+            ExampleClass2.WriteLine("Hello World!");
+        }
+    }
+}
