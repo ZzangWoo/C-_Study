2d4e3353e56efaee20489111e1933a345d26af2c
ZzangWoo <whdlwns20@gmail.com>
2020-12-21 21:50:50 +0900

    study 1221

--- Task_Example/Task_Example/Program.cs
+++ Task_Example/Task_Example/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Task_Example
 {
+    class A
+    {
+        private int privateInt;
+        protected int protectedInt;
+    }
+
+    class B : A
+    {
+        B ()
+        {
+            
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("0");
+
+            Task task = new Task(new Action(Run));
+            task.Start();
+            //task.Wait();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("----------------");
+            }
+
+            
+        }
+
+        static void Run()
+        {
+            while(true)
+            {
+                Console.WriteLine("Long running method");
+            }
         }
     }
 }
