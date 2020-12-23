36c5b42522ce52537fa8deaa6252f0215e945d91
ZzangWoo <whdlwns20@gmail.com>
2020-12-23 13:43:25 +0900

    Study 1223

--- Dynamic_Example/Dynamic_Example/Program.cs
+++ Dynamic_Example/Dynamic_Example/Program.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace Dynamic_Example
 {
+    class TestClassAA
+    {
+        public void TestMethodAA()
+        {
+            Console.WriteLine($"{this.GetType()} : AA메서드 호출");
+        }
+        public void TestMethodBB()
+        {
+            Console.WriteLine($"{this.GetType()} : BB메서드 호출");
+        }
+    }
+
+    class TestClassBB : TestClassAA { }
+
+    class TestClassCC
+    {
+        public void TestMethodAA()
+        {
+            Console.WriteLine($"{this.GetType()} : AA메서드 호출");
+        }
+        public void TestMethodBB()
+        {
+            Console.WriteLine($"{this.GetType()} : BB메서드 호출");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Example2();
+        }
+
+        private static void Example2()
+        {
+            //dynamic person = new ExpandoObject();
+        }
+
+        private static void Example1()
+        {
+            dynamic[] arr = new dynamic[] { new TestClassAA(), new TestClassBB(), new TestClassCC() };
+
+            foreach (dynamic test in arr)
+            {
+                Console.WriteLine(test.GetType());
+                test.TestMethodAA();
+                test.TestMethodBB();
+
+                Console.WriteLine();
+            }
         }
     }
 }
