fa92a81a109c33532124425f4838ddd08ce4e531
ZzangWoo <whdlwns20@gmail.com>
2020-12-24 15:08:38 +0900

    Study 1224

--- Dynamic_Example/Dynamic_Example/Program.cs
+++ Dynamic_Example/Dynamic_Example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,7 +42,50 @@ namespace Dynamic_Example
 
         private static void Example2()
         {
-            //dynamic person = new ExpandoObject();
+            // ExpandoObject에서 dynamic 타입 생성
+            dynamic person = new ExpandoObject();
+
+            // 속성 지정
+            person.Name = "Cho";
+            person.Age = 10;
+
+            // 메서드 지정
+            person.Print = (Action)(() =>
+            {
+                Console.WriteLine($"{person.Name} : {person.Age}");
+            });
+
+            // 매개변수 있는 메서드 지정
+            person.ChangeInfo = (Action<string, int>)((name, age) =>
+            {
+                person.Name = name;
+                person.Age = age;
+                if (person.InfoChanged != null)
+                {
+                    person.InfoChanged(person, EventArgs.Empty);
+                }
+            });
+
+            // 이벤트 초기화
+            person.InfoChanged = null;
+
+            // 이벤트핸들러 지정
+            person.InfoChanged += new EventHandler(OnInfoChanged);
+
+            // 매개변수로 dynamic 전달
+            DynamicTest(person);
+        }
+
+        private static void OnInfoChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Info Changed!!");
+        }
+
+        private static void DynamicTest(dynamic person)
+        {
+            person.Print();
+            person.ChangeInfo("Kim", 20);
+            person.Print();
         }
 
         private static void Example1()
